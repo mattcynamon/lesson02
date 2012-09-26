5e46343f26d05e7b122470109e06e1cb18c836ce
Matt Cynamon <mattcynamon@gmail.com>
2012-09-26 19:11:49 +0100

    mah variables

--- variables.rb
+++ variables.rb
@@ -1,9 +1,14 @@
 # 1. declare three variables and assign them odd-integer values in a single expression
-
+a, b, c = 1, 5, 7
 
 # 2. write an expression dividing two of the Task # 1 variables, but displaying the full fractional remainder
 # Hint: turn an integer into a float
 
+puts 7 / 5.to_f
+
 
 # 3. declare a name variable, assign it your name, add one of your integer variables to it, and display the result
 
+name = "Matt Cynamon"
+
+puts name + a.to_s
