08c26b18dd715a0bef8f66f2ec450f290a7e0d47
Matt Cynamon <mattcynamon@gmail.com>
2012-09-26 20:03:02 +0100

    dem hashes

--- arrays.rb
+++ arrays.rb
@@ -1,8 +1,22 @@
 # 1. create an array with three different string values
 
+ga = ["Cynamon", "CBM", "Gordon", "Nora"]
+
 # 2. add two new values to the Task #1 array in single expression
+
+ga.unshift("Rik")
+ga.push("Devin")
+
+
 
 # 3. convert your name to an array, and display it, in a single expression
 
+puts ga[0]
+
 # 4. remove and display the last value in the Task # 1 array, and discuss
 # with the instructor why both values added in Task # 2 may have been removed
+devin = ga.pop
+
+puts devin
+
+puts ga
